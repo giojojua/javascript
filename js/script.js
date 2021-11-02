a78e0dd5d59ad87b711d911aacb2fb10baf6239a
Gio <jojuagio7@gmail.com>
2021-11-02 21:40:07 +0400

    first lesson

--- js/script.js
+++ js/script.js
@@ -1,2 +1,39 @@
 'use strict';
 
+// var user = {
+//     name: "giorgi",
+//     age: 16,
+//     gender: 'male'
+// }
+
+// switch (true) {
+//     case user.age < 18:
+//         console.log("arasrulclovani");
+//         break;
+//     case user.age >= 18 && user.gender === 'male':
+//         console.log("srolculovani bichi");
+//         break;
+//     case user.gender === 'female':
+//         console.log("srulclovani gogo");
+//         break;
+//     default:
+//         console.log('undefined');
+// }
+//
+// if (user.age < 18) {
+//     console.log("arasrulclovani")
+// } else if (user.age >= 18 && user.gender === 'male') {
+//     console.log("srolculovani bichi")
+// } else if (user.gender === 'female') {
+//     console.log('srulclovani gogo')
+// }
+
+// var data = {
+//     costPrice: 32.67,
+//     sellPrice: 45.00,
+//     inventory: 1200
+// }
+//
+// var remains = data.inventory - data.costPrice + data.sellPrice
+//
+// console.log(remains)
