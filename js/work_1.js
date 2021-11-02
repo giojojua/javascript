53b7a6074380beae02dde0cabb860cfbb30bb920
Gio <jojuagio7@gmail.com>
2021-11-03 02:27:35 +0400

    first lesson

---
+++ js/work_1.js
@@ -0,0 +1,26 @@
+// TASK 1
+
+// let arr = ['javascript', 'php', 'c#', 'python', 'c++', 'kotlin', 'c', 'swift'];
+//
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i].length >= 5) {
+//         console.log(arr[i])
+//     }
+// }
+
+
+// TASK 2
+
+// let arr = [2, -3, 5, 11, 1, -30, -11, 100, -1, -3, -4];
+//
+// for (let i = 0; i < arr.length; i++) {
+//     if(arr[i] > 0) {
+//         console.log(arr[i])
+//     }
+// }
+
+
+// TASK 2
+
+let arr = [[2, -3, 5, 11], [1, -30, -11, 100], [-1, -3, -4]];
+
