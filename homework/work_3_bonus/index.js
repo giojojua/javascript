8470f6eb22c978ad85a75b86da3caa4632939065
gio <jojuagio7@gmail.com>
2021-11-15 14:32:17 +0400

    work_3 bonus completed

--- homework/work_3_bonus/index.js
+++ homework/work_3_bonus/index.js
@@ -1,22 +1,38 @@
-let sliderImages = ['/img/image1.png', '/img/image2.png', '/img/image3.png'];
-let sliderHeads = ['FOOD AND LIFE', 'COOKING', 'FRUITS'];
-let sliderMiniHeads = ['Succed in hotel business', 'Cooking delicious food', 'Getting fresh fruits']
-let sliderAbout = ['1 info', '2 info', '3 info '];
+let sliderImages = ['/img/image1.png', '/img/image2.png', '/img/image3.png', '/img/image4.jpg'];
+let sliderHeads = ['FOOD AND LIFE', 'COOKING', 'FRUITS', 'BEST PLACES'];
+let sliderMiniHeads = ['Succed in hotel business', 'Cooking delicious food', 'Getting fresh fruits', 'Diving exercises']
+let sliderAbout = ['Try it With Us', 'Try It', 'Try Muffins', 'Try Diving'];
 
-
-document.querySelectorAll('.sliderContent').forEach(slide => {
-    let slides = slide.querySelectorAll('.slide')
-    slides.forEach(slideElements => {
-        for (let i = 0; i < sliderImages.length; i++) {
-            slideElements.innerHTML = `<img src="${sliderImages[i]}" class="slideImage" alt="">
-                                    <h2 class="slideHead">${sliderHeads[i]}</h2>
-                                    <h3 class="slideMiniHead">${sliderMiniHeads[i]}</h3>
-                                    <p class="slideAbout">${sliderAbout[i]}</p>`
+function sliding() {
+    document.querySelectorAll('.slide').forEach((slideElements, index) => {
+        if (index < 3){
+            slideElements.classList.add('showSlide')
         }
+        slideElements.innerHTML = `<img src="${sliderImages[index]}" alt="">
+                            <h2>${sliderHeads[index]}</h2>
+                            <h3>${sliderMiniHeads[index]}</h3>
+                            <p>${sliderAbout[index]}</p>`
     })
+}
+
+let prevSlide = document.getElementById('left')
+
+prevSlide.addEventListener('click', function () {
+    sliderImages.unshift(sliderImages.pop())
+    sliderHeads.unshift(sliderHeads.pop())
+    sliderMiniHeads.unshift(sliderMiniHeads.pop())
+    sliderAbout.unshift(sliderAbout.pop())
+    sliding()
 })
 
-// console.log(slideImages)
-// console.log(slideHeads)
-// console.log(slideMiniHeads)
-// console.log(slideAbout)
+let nextSlide = document.getElementById('right')
+
+nextSlide.addEventListener('click', function () {
+    sliderImages.push(sliderImages.shift())
+    sliderHeads.push(sliderHeads.shift())
+    sliderMiniHeads.push(sliderMiniHeads.shift())
+    sliderAbout.push(sliderAbout.shift())
+    sliding()
+})
+
+sliding()
