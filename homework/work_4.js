aa613e9e9059dfebdec8a6292d5be0679bdd4ac4
Gio <jojuagio7@gmail.com>
2021-12-14 17:08:06 +0400

    Bonus Work

--- homework/work_4.js
+++ homework/work_4.js
@@ -1,19 +1,33 @@
-function fizzbuzz(n) {
-    for (let i = 1; i < n; i++) {
-        if (i % 3 === 0 && i % 5 === 0) {
-            console.log('fizzbuzz')
-        } else if (i % 5 === 0) {
-            console.log('buzz')
-        } else if (i % 3 === 0) {
-            console.log('fizz')
-        } else {
-            console.log(i)
-        }
-    }
+let words = ['tenet', 'inception', 'prestige', 'bob']
+
+
+function checkPalindrome(array) {
+    return array === array.split('').reverse().join('');
 }
 
-let number = 16
+const result = words.filter(word => checkPalindrome(word));
 
-fizzbuzz(number)
+console.log(result)
+
+
+// Bonus Work
+
+// function fizzbuzz(n) {
+//     for (let i = 1; i < n; i++) {
+//         if (i % 3 === 0 && i % 5 === 0) {
+//             console.log('fizzbuzz')
+//         } else if (i % 5 === 0) {
+//             console.log('buzz')
+//         } else if (i % 3 === 0) {
+//             console.log('fizz')
+//         } else {
+//             console.log(i)
+//         }
+//     }
+// }
+//
+// let number = 16
+//
+// fizzbuzz(number)
 
 // output - 1 2 fizz 4 buzz fizz 7 8 fizz buzz 13 14 fizzbuzz
