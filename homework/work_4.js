16e3a620d97ebd4f7d93bd4828494edec5811d5f
Gio <jojuagio7@gmail.com>
2021-12-14 04:10:12 +0400

    Bonus Work

--- homework/work_4.js
+++ homework/work_4.js
@@ -1,13 +1,17 @@
-let n = 16
-
-for (let i = 1; i < n; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log('fizzbuzz')
-    } else if (i % 5 === 0) {
-        console.log('buzz')
-    } else if (i % 3 === 0) {
-        console.log('fizz')
-    } else {
-        console.log(i)
+function fizzbuzz(n) {
+    for (let i = 1; i < n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('fizzbuzz')
+        } else if (i % 5 === 0) {
+            console.log('buzz')
+        } else if (i % 3 === 0) {
+            console.log('fizz')
+        } else {
+            console.log(i)
+        }
     }
 }
+
+let number = 16
+
+fizzbuzz(number)
